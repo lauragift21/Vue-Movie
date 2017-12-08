5583810674f270c6a720f941f2634b58c1b95450
egwuenu gift <laurandidi21@gmail.com>
2017-12-08 07:39:09 +0100

    Allow poster image to display

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -10,22 +10,21 @@
       <button class="btn search_area" @click="searchMovie">Search</button>     
     </div>
     <div class="result">
-        <div class="card">
-          <div class="movie" v-for="movie in movies" :key="movie.id">
-            <div class="movie__poster">
-              <img v-if="!noImage"  :src=" url + movie.poster_path" alt="" class="poster">
-              <img v-if="noImage" src="assets/no-image.png">  
-            </div>
-            <div class="movie__title">
-              <h3 style="color: #000">
-                {{ movie.title }}            
-              </h3>
-            </div>
-            <div class="movie__date">
-              <p style="color: #333;">{{ movie.release_date}}</p>            
-            </div>
-
+      <div class="card">
+        <div class="movie" v-for="movie in movies" :key="movie.id">
+          <div class="movie__poster">
+            <img v-if="!noImage"  :src="url + movie.poster_path" alt="" class="poster">
+            <img v-if="noImage" src="src/assets/no-image.png" height="2%">  
           </div>
+          <div class="movie__title">
+            <h4 style="color: #333">
+              {{ movie.title }}            
+            </h4>
+          </div>
+          <div class="movie__date">
+            <p style="color: #333; font-size: 12px">{{ movie.release_date}}</p>            
+          </div>
+        </div>
         </div>
       </div>
     </div>
@@ -37,20 +36,22 @@
   import _ from 'lodash'
   
   export default {
+    prop: ['movie'],
     data() {
       return {
         query: '',
         movies: [],
         noImage: false,
-        url: 'https://image.tmdb.org/t/p/w185/'
+        url: 'http://image.tmdb.org/t/p/w185//'
       }
+    },
+    computed: {
     },
     methods: {
       searchMovie(e) {
-        console.log(this.query);
         const api = `https://api.themoviedb.org/3/search/movie?api_key=8e3003c0c81633dc53b9d15ffa3399e1&language=en-US&query=${this.query}&page=1&include_adult=false`;
         axios.get(api).then(response => {
-          this.movies = response.data.results;
+          this.movies = response.data.results;          
         })
         .catch (e => {
           this.error = 'No response data'
